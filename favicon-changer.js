7f89a2857bfc0e9f7a51c8ac1a3e8daaccd13446
pumpkin <60913876+mojaev-pp@users.noreply.github.com>
2026-03-19 20:06:39 +0300

    Update favicon-changer.js

--- favicon-changer.js
+++ favicon-changer.js
@@ -1,5 +1,5 @@
 // favicon-changer.js
-if (window.location.pathname.includes('/help/')) {
+if (window.location.pathname.includes('/help')) {
   const link = document.querySelector("link[rel='icon']") || document.createElement('link');
   link.rel = 'icon';
   link.href = '/1.png';
